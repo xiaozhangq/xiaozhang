3020341b6fc17f6d77bf8e06211e724c6bdc973b
xiaozhangq <88882729+xiaozhangq@users.noreply.github.com>
2026-03-02 10:55:01 +0800

    Create main XiaoZhangApplication.java SpringBoot main class

---
+++ backend/src/main/java/com/xiaozhang/XiaoZhangApplication.java
@@ -0,0 +1,12 @@
+package com.xiaozhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoZhangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoZhangApplication.class, args);
+    }
+}
